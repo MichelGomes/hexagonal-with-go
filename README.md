c3dd5f03c88a213997bab547b1e1c7ec1a11aa6d
Michel Gomes <gomes.michel@gmail.com>
2021-03-30 22:39:36 -0300

    fix: Correção no nomenclatura do mocks

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ INSTALL MOCKGEN
 go install github.com/golang/mock/mockgen@v1.5.0
 
 CREATE MOCKS 
-mockgen -destination=application/mocks/application -source=application/product.go application
+mockgen -destination=application/mocks/application -source=application/product.go application.go
--- application/product_service_test.go
+++ application/product_service_test.go
@@ -3,7 +3,10 @@ package application_test
 import (
 	"testing"
 
+	"github.com/MichelGomes/hexagonal-with-go/application"
+	mock_application "github.com/MichelGomes/hexagonal-with-go/application/mocks"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
 )
 
 func TestProductService_Get(t *testing.T) {
@@ -11,5 +14,16 @@ func TestProductService_Get(t *testing.T) {
 	//Prolonga a execução até que os demais comandos subquesentes terminem
 	defer ctrl.Finish()
 
-	//product := mock
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockPersistenceInterface(ctrl)
+
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
 }
